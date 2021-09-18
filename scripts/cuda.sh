a16300cc83af8229e4d177fe9df8809e14546596
awesome-doge <awesomedogewow@gmail.com>
2021-09-19 03:39:02 +0800

    添加cuda 註釋

--- scripts/cuda.sh
+++ scripts/cuda.sh
@@ -4,14 +4,20 @@ if [ "$(id -u)" != "0" ]; then
 	echo "Please run script as root"
 	exit 1
 fi
+# 顏色定義
+COLOR='\033[92m'
+ENDC='\033[0m'
 # 安裝 依賴套件
+echo -e "${COLOR}[1]${ENDC} 安裝 依賴套件"
 apt install -y build-essential git make cmake clang libgflags-dev zlib1g-dev libssl-dev libreadline-dev libmicrohttpd-dev pkg-config libgsl-dev python3 python3-dev python3-pip
 pip3 install psutil crc16 requests
 # 移除舊的
+echo -e "${COLOR}[2]${ENDC} 移除舊的"
 rm -rf /usr/bin/ton/
 rm -rf /usr/src/ton/
 rm -rf /usr/src/pow-miner-gpu/
 # 下載 pow-miner-gpu
+echo -e "${COLOR}[3]${ENDC} 下載 pow-miner-gpu"
 cd /usr/src
 git clone --recursive https://github.com/tontechio/pow-miner-gpu.git
 cd /usr/src/pow-miner-gpu
@@ -19,6 +25,7 @@ git checkout main
 cd /usr/src/
 mv pow-miner-gpu ton
 # 初始化編譯環境
+echo -e "${COLOR}[4]${ENDC} 初始化編譯環境"
 mkdir /usr/bin/ton
 cd /usr/bin/ton
 export CCACHE_DISABLE=1
@@ -30,5 +37,6 @@ cmake -DCMAKE_BUILD_TYPE=Release -DMINERCUDA=true /usr/src/ton
 memory=$(cat /proc/meminfo | grep MemAvailable | awk '{print $2}')
 let "cpuNumber = memory / 2100000"
 # 編譯
+echo -e "${COLOR}[5]${ENDC} 編譯pow-miner-cuda lite-client"
 make -j ${cpuNumber} pow-miner-cuda lite-client
 cd ~
