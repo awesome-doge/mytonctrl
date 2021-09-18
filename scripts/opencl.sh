02db09d19617cfdb0c36cd8e9d4a8c13cf919347
awesome-doge <awesomedogewow@gmail.com>
2021-09-19 03:25:32 +0800

    添加 opencl

--- scripts/opencl.sh
+++ scripts/opencl.sh
@@ -4,14 +4,20 @@ if [ "$(id -u)" != "0" ]; then
 	echo "Please run script as root"
 	exit 1
 fi
+# 顏色定義
+COLOR='\033[92m'
+ENDC='\033[0m'
 # 安裝 依賴套件
+echo -e "${COLOR}[1]${ENDC} 安裝 依賴套件"
 apt install -y build-essential git make cmake clang libgflags-dev zlib1g-dev libssl-dev libreadline-dev libmicrohttpd-dev pkg-config libgsl-dev python3 python3-dev python3-pip
 pip3 install psutil crc16 requests
 # 移除舊的
+echo -e "${COLOR}[2]${ENDC} 移除舊的"
 rm -rf /usr/bin/ton/
 rm -rf /usr/src/ton/
 rm -rf /usr/src/pow-miner-gpu/
 # 下載 pow-miner-gpu
+echo -e "${COLOR}[3]${ENDC} 下載 pow-miner-gpu"
 cd /usr/src
 git clone --recursive https://github.com/tontechio/pow-miner-gpu.git
 cd /usr/src/pow-miner-gpu
@@ -19,8 +25,10 @@ git checkout main
 cd /usr/src/
 mv pow-miner-gpu ton
 # opencl
-apt-get install opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev
+echo -e "${COLOR}[4]${ENDC} 安裝opencl"
+apt-get install -y opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev
 # 初始化編譯環境
+echo -e "${COLOR}[5]${ENDC} 初始化編譯環境"
 mkdir /usr/bin/ton
 cd /usr/bin/ton
 export CCACHE_DISABLE=1
@@ -29,5 +37,6 @@ cmake -DCMAKE_BUILD_TYPE=Release -DMINEROPENCL=true /usr/src/ton
 memory=$(cat /proc/meminfo | grep MemAvailable | awk '{print $2}')
 let "cpuNumber = memory / 2100000"
 # 編譯
+echo -e "${COLOR}[6]${ENDC} 編譯pow-miner-opencl lite-client"
 make -j ${cpuNumber} pow-miner-opencl lite-client
 cd ~
