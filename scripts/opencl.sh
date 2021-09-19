b472509e87f08a43f9b51814d90ba238ea7ed375
awesome-doge <awesomedogewow@gmail.com>
2021-09-19 20:56:11 +0800

    添加 fift

--- scripts/opencl.sh
+++ scripts/opencl.sh
@@ -38,5 +38,5 @@ memory=$(cat /proc/meminfo | grep MemAvailable | awk '{print $2}')
 let "cpuNumber = memory / 2100000"
 # 編譯
 echo -e "${COLOR}[6]${ENDC} 編譯pow-miner-opencl lite-client"
-make -j ${cpuNumber} pow-miner-opencl lite-client
+make -j ${cpuNumber} pow-miner-opencl lite-client fift
 cd ~
