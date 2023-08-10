d1760f83cfd53f307791ea5110be73825cfc8150
Doge <awesomedogewow@gmail.com>
2023-08-11 02:46:06 +0800

    Refactor xrestart.py for better readability and maintainability
    - Refactored the `Xguard` function to `guard` for clarity.
    - Added docstrings to the `guard`, `execute_command`, and `demote` functions.
    - Renamed variables for better understanding (`timenow` to `current_time`, etc.).
    - Replaced hard-coded file path with a constant variable (`SERVICE_FILE_PATH`).
    - Moved the logic to read and parse the service file into a context manager.
    - Improved comments and removed unnecessary print statements.
    
    This commit simplifies the code structure, improves code readability, and enhances maintainability.

--- scripts/xrestart.py
+++ scripts/xrestart.py
@@ -1,73 +1,76 @@
-import os
 import sys
 import pwd
 import time
-import threading
 import subprocess
+from pathlib import Path
 
-def Xguard():
-	timestamp = int(sys.argv[1])
-	args = sys.argv[2:]
-	while True:
-		time.sleep(1)
-		timenow = int(time.time())
-		if timenow > timestamp:
-			Xcmd(args)
-			print("exit")
-			sys.exit(0)
-#end define
+SERVICE_FILE_PATH = Path("/etc/systemd/system/validator.service")
 
-def Xcmd(inputArgs):
-	print("inputArgs:", inputArgs)
-	
-	# stop validator
-	args = ["systemctl", "stop", "validator"]
-	subprocess.run(args)
-	
-	file = open("/etc/systemd/system/validator.service", 'rt')
-	text = file.read()
-	file.close()
-	lines = text.split('\n')
-	
-	for line in lines:
-		if "ExecStart" not in line:
-			continue
-		ExecStart = line.replace("ExecStart = ", '')
-		args = ExecStart.split(' ')
-		print("ExecStart args:", args)
-		args += inputArgs
-	#end for
+def guard():
+    """
+    Periodically check the timestamp and execute the command after the timestamp is surpassed.
+    """
+    timestamp = int(sys.argv[1])
+    args = sys.argv[2:]
+    while True:
+        time.sleep(1)
+        current_time = int(time.time())
+        if current_time > timestamp:
+            execute_command(args)
+            print("exit")
+            sys.exit(0)
 
-	pw_record = pwd.getpwnam("validator")
-	user_uid = pw_record.pw_uid
-	user_gid = pw_record.pw_gid
-	
-	# start with args
-	print("args:", args)
-	process = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=demote(user_uid, user_gid))
-	process.wait()
-	text = process.stdout.read().decode()
-	print("text:", text)
-	
-	# Exit program
-	sys.exit(0)
-#end define
+def execute_command(input_args):
+    """
+    Execute the command with the specified arguments.
+    
+    :param input_args: List of input arguments to be appended to the ExecStart command.
+    """
+    print("inputArgs:", input_args)
+    
+    # Stop validator
+    subprocess.run(["systemctl", "stop", "validator"])
+
+    with SERVICE_FILE_PATH.open('r') as file:
+        text = file.read()
+        for line in text.split('\n'):
+            if "ExecStart" in line:
+                exec_start = line.replace("ExecStart = ", '').split(' ')
+                print("ExecStart args:", exec_start)
+                args = exec_start + input_args
+
+    pw_record = pwd.getpwnam("validator")
+    user_uid = pw_record.pw_uid
+    user_gid = pw_record.pw_gid
+    
+    # Start with args
+    print("args:", args)
+    process = subprocess.Popen(
+        args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE, preexec_fn=demote(user_uid, user_gid)
+    )
+    process.wait()
+    output_text = process.stdout.read().decode()
+    print("Output:", output_text)
+    
+    # Exit program
+    sys.exit(0)
 
 def demote(user_uid, user_gid):
-	def result():
-		os.setgid(user_gid)
-		os.setuid(user_uid)
-		os.system("ulimit -n 1")
-		os.system("ulimit -u 1")
-		os.system("ulimit -l 1")
-	return result
-#end define
-
-
-###
-### Start of the program
-###
+    """
+    Return a function to demote the process to the specified user ID and group ID.
+    
+    :param user_uid: User ID to demote to.
+    :param user_gid: Group ID to demote to.
+    :return: A function that performs the demotion.
+    """
+    def result():
+        os.setgid(user_gid)
+        os.setuid(user_uid)
+        os.system("ulimit -n 1")
+        os.system("ulimit -u 1")
+        os.system("ulimit -l 1")
+    return result
 
 if __name__ == "__main__":
-	Xguard()
-#end if
+    guard()
