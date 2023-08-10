42a188ad61594e0bd75eaa9b5f91e74e8e5dd63e
Doge <awesomedogewow@gmail.com>
2023-08-11 02:30:30 +0800

    feat: Improve jsonrpcinstaller.sh script
    - Set `set -euo pipefail` to ensure script execution stops on errors
    - Added check for root permissions and exit with error message if not running as root
    - Added default parameters and ANSI color codes for better readability
    - Improved argument parsing and added validation for the user argument
    - Refactored installation steps into a function to improve code organization
    - Updated comments and output messages for clarity

--- scripts/jsonrpcinstaller.sh
+++ scripts/jsonrpcinstaller.sh
@@ -1,40 +1,58 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
-# Проверить sudo
-if [ "$(id -u)" != "0" ]; then
-	echo "Please run script as root"
-	exit 1
+# Ensure script is run with root permissions
+if [[ "$(id -u)" != "0" ]]; then
+    echo "Error: This script must be run as root."
+    exit 1
 fi
 
-# Get arguments
-while getopts u: flag
-do
-	case "${flag}" in
-		u) user=${OPTARG};;
-	esac
+# Default parameters
+USER=""
+
+# ANSI color codes
+MAGENTA='\033[95m'
+RESET='\033[0m'
+
+# Parse arguments
+while getopts u: option; do
+    case "${option}" in
+        u) USER=${OPTARG} ;;
+    esac
 done
 
-# Цвета
-COLOR='\033[95m'
-ENDC='\033[0m'
+# Validate user argument
+if [[ -z "${USER}" ]]; then
+    echo "Error: Missing -u (user) argument."
+    exit 1
+fi
 
-# Установка компонентов python3
-echo -e "${COLOR}[1/4]${ENDC} Installing required packages"
+# Function to print step
+print_step() {
+    echo -e "${MAGENTA}[$1/4]${RESET} $2"
+}
+
+# Install python3 components
+print_step 1 "Installing required packages"
 pip3 install Werkzeug json-rpc cloudscraper pyotp
 
-# Клонирование репозиториев с github.com
-echo -e "${COLOR}[2/4]${ENDC} Cloning github repository"
-cd /usr/src/
-rm -rf mtc-jsonrpc
-git clone --recursive https://github.com/igroman787/mtc-jsonrpc.git
+# Clone repositories from GitHub
+print_step 2 "Cloning github repository"
+REPO_DIR="/usr/src/mtc-jsonrpc"
+if [[ -d "${REPO_DIR}" ]]; then
+    rm -rf "${REPO_DIR}"
+fi
+git clone --recursive https://github.com/igroman787/mtc-jsonrpc.git "${REPO_DIR}"
 
-# Прописать автозагрузку
-echo -e "${COLOR}[3/4]${ENDC} Add to startup"
-cmd="from sys import path; path.append('/usr/src/mytonctrl/'); from mypylib.mypylib import *; Add2Systemd(name='mtc-jsonrpc', user='${user}', start='/usr/bin/python3 /usr/src/mtc-jsonrpc/mtc-jsonrpc.py')"
-python3 -c "${cmd}"
+# Add to system startup
+print_step 3 "Add to startup"
+PYTHON_CMD="from sys import path
+path.append('/usr/src/mytonctrl/')
+from mypylib.mypylib import *
+Add2Systemd(name='mtc-jsonrpc', user='${USER}', start='/usr/bin/python3 /usr/src/mtc-jsonrpc/mtc-jsonrpc.py')"
+python3 -c "${PYTHON_CMD}"
 systemctl restart mtc-jsonrpc
 
-# Выход из программы
-echo -e "${COLOR}[4/4]${ENDC} JsonRPC installation complete"
+# Completion message
+print_step 4 "JsonRPC installation complete"
 exit 0
