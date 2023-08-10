8fa050e2ae391fe80bb037e4fa705a40768987af
Doge <awesomedogewow@gmail.com>
2023-08-11 02:46:20 +0800

    Refactor service file modification and systemctl daemon reload
    This commit refactors the code for modifying the content of a service file and reloading the systemctl daemon. The changes include:
    - Reorganizing the code into functions for better readability and maintainability
    - Using pathlib.Path instead of os.path for file operations
    - Adding docstrings to functions for documentation purposes
    
    The main function now orchestrates the updating of the service file and reloading the systemctl daemon.

--- scripts/upgrade.py
+++ scripts/upgrade.py
@@ -1,31 +1,72 @@
-# Говнокод ON
-import os
-from sys import path
-from os.path import dirname as dir
-path.append(dir(path[0]))
-# Говнокод OFF
+import subprocess
+from pathlib import Path
 
-from mypylib.mypylib import *
+# Constants
+SERVICE_FILE_PATH = Path("/etc/systemd/system/validator.service")
+SEARCH_TEXT = "validator-engine/ton-global.config.json"
+REPLACE_TEXT = "global.config.json"
 
 
-# validator.service
-file = open("/etc/systemd/system/validator.service", 'rt')
-text = file.read()
-file.close()
-lines = text.split('\n')
+def read_file(file_path: Path) -> str:
+    """
+    Read the content of a file.
+    
+    :param file_path: The path of the file to be read.
+    :return: Content of the file.
+    """
+    with file_path.open('r') as file:
+        return file.read()
 
-for i in range(len(lines)):
-	line = lines[i]
-	if "ExecStart" not in line:
-		continue
-	if "ton-global.config.json" in line:
-		lines[i] += line.replace("validator-engine/ton-global.config.json", "global.config.json")
-#end for
 
-text = "\n".join(lines)
-file = open("/etc/systemd/system/validator.service", 'wt')
-file.write(text)
-file.close()
+def write_file(file_path: Path, content: str) -> None:
+    """
+    Write the content to a file.
+    
+    :param file_path: The path of the file to be written.
+    :param content: The content to be written to the file.
+    """
+    with file_path.open('w') as file:
+        file.write(content)
 
-args = ["systemctl", "daemon-reload"]
-subprocess.run(args)
+
+def modify_service_file_content(content: str) -> str:
+    """
+    Modify the content of the service file by replacing specific text.
+    
+    :param content: The original content of the service file.
+    :return: Modified content.
+    """
+    lines = content.split('\n')
+    modified_lines = [
+        line.replace(SEARCH_TEXT, REPLACE_TEXT) if "ExecStart" in line and SEARCH_TEXT in line else line
+        for line in lines
+    ]
+    return "\n".join(modified_lines)
+
+
+def update_service_file():
+    """
+    Update the content of the service file with modifications.
+    """
+    content = read_file(SERVICE_FILE_PATH)
+    modified_content = modify_service_file_content(content)
+    write_file(SERVICE_FILE_PATH, modified_content)
+
+
+def reload_systemctl_daemon():
+    """
+    Reload the systemctl daemon.
+    """
+    subprocess.run(["systemctl", "daemon-reload"])
+
+
+def main():
+    """
+    Main function to orchestrate the updating of the service file and reload the systemctl daemon.
+    """
+    update_service_file()
+    reload_systemctl_daemon()
+
+
+if __name__ == "__main__":
+    main()
